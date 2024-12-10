63cb9b837d9391de46ff77f3ad59110741a0ca10
Lilly1812 <hoangphuongly00@gmail.com>
2024-12-10 17:01:53 +0700

    dfg

--- Styles/NavigateBar.xaml.cs
+++ Styles/NavigateBar.xaml.cs
@@ -24,5 +24,31 @@ namespace OOP_EventsManagementSystem.Styles
         {
             InitializeComponent();
         }
+
+        private void AccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Kiểm tra xem ContextMenu có xuất hiện không khi nhấn nút
+            MessageBox.Show("Nút đã được nhấn");
+            AccountContextMenu.IsOpen = true;
+        }
+
+        private void ViewAccount_Click(object sender, RoutedEventArgs e)
+        {
+            // Xử lý logic khi người dùng chọn "Xem thông tin tài khoản"
+            MessageBox.Show("Xem thông tin tài khoản");
+        }
+
+        private void EditAccount_Click(object sender, RoutedEventArgs e)
+        {
+            // Xử lý logic khi người dùng chọn "Chỉnh sửa tài khoản"
+            MessageBox.Show("Chỉnh sửa tài khoản");
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            // Xử lý logic khi người dùng chọn "Log Out"
+            MessageBox.Show("Đăng xuất");
+        }
+
     }
 }
--- ViewModel/EventVM.cs
+++ ViewModel/EventVM.cs
@@ -1,53 +1,12 @@
-﻿using OOP_EventsManagementSystem.View;
-using System.Windows.Controls;
-using System.Windows.Input;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace OOP_EventsManagementSystem.ViewModel
 {
-    public class EventVM
+    internal class EventVM
     {
-        public string ImagePath { get; set; }
-        public string Day { get; set; }
-        public string Month { get; set; }
-        public string Weekday { get; set; }
-        public string Name { get; set; }
-
-        public ICommand ItemSelectedCommand { get; set; }
-
-        public EventVM()
-        {
-            // Khởi tạo lệnh, khi người dùng nhấn vào sự kiện, hàm OnItemSelected sẽ được gọi
-            ItemSelectedCommand = new RelayCommand(OnItemSelected);
-        }
-
-        private void OnItemSelected(object parameter)
-        {
-            if (parameter is EventVM eventVM)
-            {
-                // Log thông tin sự kiện đã chọn
-                Console.WriteLine($"{eventVM.Name} đã được chọn!");
-
-                // Tìm Frame trong cửa sổ chính
-                var frame = Application.Current.MainWindow.FindName("MainFrame") as Frame;
-                if (frame != null)
-                {
-                    // Tạo đối tượng trang chi tiết sự kiện
-                    var eventDetails = new EventDetails
-                    {
-                        DataContext = new EventDetailsVM(
-                            eventVM.Name,
-                            eventVM.Day,
-                            eventVM.Month,
-                            eventVM.Weekday,
-                            eventVM.ImagePath)
-                    };
-
-                    // Điều hướng đến trang chi tiết sự kiện
-                    frame.Navigate(eventDetails);
-                }
-            }
-        }
-
-
     }
 }
