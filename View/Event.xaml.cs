4e715ffd9c429d1d9f8629ac7d8f0da28db34639
hoangnamgithub <nguyenhoangnam0437@gmail.com>
2024-12-14 17:58:52 +0700

    bind

--- View/Event.xaml.cs
+++ View/Event.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOP_EventsManagementSystem.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace OOP_EventsManagementSystem.View
         public Event()
         {
             InitializeComponent();
+            DataContext = new EventVM();
         }
     }
 }
--- ViewModel/EventVM.cs
+++ ViewModel/EventVM.cs
@@ -9,11 +9,38 @@ using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+using OOP_EventsManagementSystem.Model;
+using Microsoft.Identity.Client;
 
 
 namespace OOP_EventsManagementSystem.ViewModel
 {
-    internal class EventVM
+    internal class EventVM : INotifyPropertyChanged
     {
+        private readonly EventManagementDbContext _context;
+
+        public ObservableCollection<Model.Event> Events { get; set; }
+
+        public EventVM()
+        {
+            _context = new EventManagementDbContext();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Events = new ObservableCollection<Model.Event>(_context.Events.Include(e => e.Venue).ToList()); 
+            OnPropertyChanged(nameof(Events));
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
