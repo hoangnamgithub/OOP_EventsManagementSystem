28a23179d1574a76cb0c0cf3b38306cd10084f70
hoangnamgithub <nguyenhoangnam0437@gmail.com>
2024-12-15 23:28:04 +0700

    15_12 ok

--- ViewModel/EquipmentVM.cs
+++ ViewModel/EquipmentVM.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using OOP_EventsManagementSystem.Model;
+using OOP_EventsManagementSystem.Styles;
+using OOP_EventsManagementSystem.Utilities;
+using OOP_EventsManagementSystem.View;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace OOP_EventsManagementSystem.ViewModel
 {
-    class EquipmentVM
+    class EquipmentVM : INotifyPropertyChanged
     {
+        private readonly EventManagementDbContext _context;
+
+        public ObservableCollection<Model.Equipment> Equipments { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
