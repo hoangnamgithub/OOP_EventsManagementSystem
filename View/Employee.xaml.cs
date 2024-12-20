5357a4a2caad796471136bbf2ac9941c3460e45b
hoangnamgithub <nguyenhoangnam0437@gmail.com>
2024-12-20 14:45:13 +0700

    .

--- View/Employee.xaml.cs
+++ View/Employee.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OOP_EventsManagementSystem.Model;
 using OOP_EventsManagementSystem.ViewModel;
 
 namespace OOP_EventsManagementSystem.View
--- ViewModel/EmployeeVM.cs
+++ ViewModel/EmployeeVM.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Microsoft.EntityFrameworkCore;
+using OOP_EventsManagementSystem.Model;
 using OOP_EventsManagementSystem.Styles;
 using OOP_EventsManagementSystem.Utilities;
 
@@ -13,16 +16,44 @@ namespace OOP_EventsManagementSystem.ViewModel
 {
     class EmployeeVM : INotifyPropertyChanged
     {
+
+        private readonly EventManagementDbContext _context;
+        public ObservableCollection<EmployeeDisplayModel> TodayEmployees { get; set; }
         public ICommand AddCommand { get; }
         public ICommand EditCommand { get; }
         public ICommand DeleteCommand { get; }
 
-        public EmployeeVM() 
+        public EmployeeVM(EventManagementDbContext context) 
         {
             AddCommand = new RelayCommand(AddNewEmployee);
             EditCommand = new RelayCommand(EditEmployee);
             DeleteCommand = new RelayCommand(DeleteEmployee);
 
+            _context = context; 
+            LoadData();
+
+        }
+
+        private void LoadData()
+        {
+            var today = DateTime.Today;
+            var engagedToday = _context.Engageds
+                .Include(e => e.Employee)
+                .Include(e => e.Event)
+                .Include(e => e.Employee.Role)
+                .Where(e => e.Event.StartDate.ToDateTime(TimeOnly.MinValue) <= today &&
+                            e.Event.EndDate.ToDateTime(TimeOnly.MinValue) >= today)
+                .Select(e => new EmployeeDisplayModel
+                {
+                    FullName = e.Employee.FullName,
+                    Contact = e.Employee.Contact,
+                    RoleName = e.Employee.Role.RoleName,
+                    EventName = e.Event.EventName
+                })
+                .ToList();
+
+            TodayEmployees = new ObservableCollection<EmployeeDisplayModel>(engagedToday);
+            OnPropertyChanged(nameof(TodayEmployees));
         }
 
         private void AddNewEmployee (Object parameter) 
@@ -46,4 +77,12 @@ namespace OOP_EventsManagementSystem.ViewModel
         }
 
     }
+    public class EmployeeDisplayModel
+    {
+        public string FullName { get; set; }
+        public string Contact { get; set; }
+        public string RoleName { get; set; }
+        public string EventName { get; set; }
+    }
+
 }
