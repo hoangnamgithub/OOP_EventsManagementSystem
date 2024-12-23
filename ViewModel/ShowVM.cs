31bed6c7ad3ac6f7662d503fd5474c9f5e56c6f8
Lilly1812 <hoangphuongly00@gmail.com>
2024-12-24 00:12:24 +0700

    10% show

--- ViewModel/ShowVM.cs
+++ ViewModel/ShowVM.cs
@@ -1,7 +1,9 @@
-﻿using OOP_EventsManagementSystem.Styles;
+﻿using OOP_EventsManagementSystem.Model;
+using OOP_EventsManagementSystem.Styles;
 using OOP_EventsManagementSystem.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -12,30 +14,35 @@ namespace OOP_EventsManagementSystem.ViewModel
 {
     class ShowVM : INotifyPropertyChanged
     {
-        
-        public ICommand EditCommand { get; set; }
-        public ICommand DeleteCommand { get; set; }
-        
-
-        public ShowVM() 
+        private ObservableCollection<Performer> _performers;
+        public ObservableCollection<Performer> Performers
         {
-            
-            EditCommand = new RelayCommand(EditShowDetails);
-            DeleteCommand = new RelayCommand(DeleteShowDetails);
-            
+            get { return _performers; }
+            set
+            {
+                _performers = value;
+                OnPropertyChanged(nameof(Performers)); // Tạo phương thức OnPropertyChanged trong BaseViewModel để thông báo thay đổi
+            }
         }
 
-        
-        private void EditShowDetails(object obj)
-        {
-            
-        }
-        private void DeleteShowDetails(object obj)
-        {
-            
-        }
-       
+        private readonly EventManagementDbContext _context;
 
+        public ShowVM()
+        {
+            _context = new EventManagementDbContext();
+            Performers = new ObservableCollection<Performer>();
+        }
+
+        // Sử dụng ToList thay vì ToListAsync
+        public void LoadPerformers()
+        {
+            var performersFromDb = _context.Performers.ToList(); // Sử dụng ToList() thay vì ToListAsync()
+            Performers.Clear();
+            foreach (var performer in performersFromDb)
+            {
+                Performers.Add(performer);
+            }
+        }
         // Implementation of INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName)
